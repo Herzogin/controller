8c0cbdb034778193ab41c318d46f099d5c8dfcb8
Malwine <malwine@mailbox.org>
2016-12-20 13:08:08 +0100

    Attempt to start server

--- src/lampButtonServer/Server.java
+++ src/lampButtonServer/Server.java
@@ -1,12 +1,28 @@
 package lampButtonServer;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 
 public class Server implements ServerInterface {
+
+	public Server() {
+		try {
+			Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT); 
+			registry.bind("Server", this);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
 	@Override
 	public void sendMessage(String message) throws RemoteException {
 		System.out.println("Message received: " + message);
+	}
+	
+	public static void main(String[] args) {
+		new Server();
 	}
 	
 }
