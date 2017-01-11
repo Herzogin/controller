f33fc3e7845a9e9bf53afa0c02ad3addf99bbad1
Malwine <malwine@mailbox.org>
2017-01-11 23:27:18 +0100

    Enable many services to be registered and store lamps in lampgroup

--- src/lampButtonServer/Controller.java
+++ src/lampButtonServer/Controller.java
@@ -7,9 +7,12 @@ import buttonLampInterfaces.ControllerInterface;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller extends java.rmi.server.UnicastRemoteObject implements ControllerInterface {
 	private LampInterface li = null;
+	private List<LampInterface> lampGroup = new ArrayList<LampInterface>();
 
 	public Controller() throws RemoteException {
 		super();
@@ -18,13 +21,25 @@ public class Controller extends java.rmi.server.UnicastRemoteObject implements C
 	public void start() {
 		try {
 			Registry registry = LocateRegistry.createRegistry(3000/*Registry.REGISTRY_PORT*/);
+			
+			Thread.sleep(20000); // pause this program until we started all the button and lamp services
+			
 			String[] list = registry.list();
 			
-			Thread.sleep(30000);
-			ButtonInterface bi = (ButtonInterface) registry.lookup("button");
-			bi.register(this);
-			
-			li = (LampInterface) registry.lookup("lamp");
+			for (String item : list ) {
+				
+				if (item.contains("button")) {
+					ButtonInterface bi = (ButtonInterface) registry.lookup(item);
+					bi.register(this);
+					System.out.println("button was registered: " + item);
+					
+				} else if (item.contains("lamp")) {
+					li = (LampInterface) registry.lookup(item);
+					
+					lampGroup.add(li); // we save every lamp item in one lampgroup, which we will iterate on later
+					System.out.println("lamp was registered: " + item);
+				}
+			}
 			
 		} catch (Exception e) {
 			System.out.println(e);
